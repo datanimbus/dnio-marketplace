10b88f233c9ed7ca0c5cb4c0ef8cab70fb424085
Jerry M <developer.jerrymannel@gmail.com>
2024-06-26 04:49:55 +0530

    added checks

--- MongoDB/node.insertOne.js
+++ MongoDB/node.insertOne.js
@@ -4,8 +4,15 @@ module.exports = async (connectorData, inputData) => {
 	logger.trace("MongoDB InsertOne Node: Invoked!");
 	try {
 		logger.trace(`MongoDB InsertOne Node: input - ${JSON.stringify(inputData)}`);
+		if (!inputData.collection) {
+			logger.error(`MongoDB InsertOne Node: Collection name is required`);
+			throw {
+				"code": "MONGODB_INSERT_ONE_ERROR",
+				"message": "Collection name is required"
+			}
+		}
 		const result = await connectorData.db.collection(inputData.collection).insertOne(inputData.data);
-		logger.trace(`MongoDB InsertOne Node: Insert successful`);
+		logger.debug(`MongoDB InsertOne Node: Insert successful`);
 		logger.trace(`MongoDB InsertOne Node: Insert response : ${JSON.stringify(result)}`);
 		return { insertedId: result.insertedId };
 	} catch (error) {
