41b391e3c7f3e36e93b3a50aa402dd4467551bfd
nimesh_dools <nimesh@datanimbus.com>
2024-06-26 14:34:37 -0500

    rename and handle empty queries and values

---
+++ PostgreSQL/node.sqlBuilder.js
@@ -0,0 +1,21 @@
+let logger = global.logger;
+
+module.exports = async (connectorData, inputData) => {
+	logger.trace("PostgreSQL SQL Builder Node: Invoked!");
+	try {
+		logger.trace(`PostgreSQL SQL Builder Node: input - ${JSON.stringify(inputData)}`);
+		const query = inputData.query ?? '';
+		const values = inputData.values ?? [];
+		const result = await connectorData.pgCon.query(query, values);
+		logger.trace(`PostgreSQL SQL Builder Node: SQL Builder successful`);
+		logger.trace(`PostgreSQL SQL Builder Node: SQL Builder response : ${JSON.stringify(result)}`);
+		return { rowCount: result.rowCount, rows: result.rows };
+	} catch (error) {
+		logger.error(`PostgreSQL SQL Builder Node: Error executing SQL Builder query in PostgreSQL: ${error}`);
+		throw {
+			"code": "POSTGRESQL_SQL_BUILDER_ERROR",
+			"message": "Error executing SQL Builder query in PostgreSQL",
+			"stackTrace": error
+		}
+	}
+}
