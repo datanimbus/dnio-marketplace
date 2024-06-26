6580b7bc38a908ccf0e43b97c0a8376a35af182e
nimesh_dools <nimesh@datanimbus.com>
2024-06-26 15:43:00 -0500

    rename file and handle empty queries

---
+++ MySQL/node.sqlBuilder.js
@@ -0,0 +1,24 @@
+let logger = global.logger;
+
+module.exports = async (connectorData, inputData) => {
+	logger.trace("MySQL SQL Builder: Invoked!");
+	try {
+		logger.trace(`MySQL SQL Builder: input - ${JSON.stringify(inputData)}`);
+		if (!inputData.query) {
+			throw { message: "SQL Query is required." };
+		}
+		const values = inputData.values ?? [];
+
+		const [result, _] = await connectorData.MySQLCon.execute(inputData.query, values);
+		logger.trace(`MySQL SQL Builder: SQL Builder successful`);
+		logger.trace(`MySQL SQL Builder: SQL Builder response : ${JSON.stringify(result)}`);
+		return result;	
+	} catch (error) {
+		logger.error(`MySQL SQL Builder: Error executing SQL Builder query in MySQL: ${error}`);
+		throw {
+			"code": "MYSQL_SQL_BUILDER_ERROR",
+			"message": "Error executing SQL Builder query in MySQL",
+			"stackTrace": error
+		}
+	}
+}
