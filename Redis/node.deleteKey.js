435e8da7924eb00a44c952a4786fdc6ca76860ab
nimesh_dools <nimesh@datanimbus.com>
2024-07-02 15:46:05 -0500

    redis delete key node

---
+++ Redis/node.deleteKey.js
@@ -0,0 +1,17 @@
+let logger = global.logger;
+
+module.exports = async (connectorData, inputData) => {
+    logger.trace("Redis Delete Value Node: Invoked!");
+    try {
+        logger.trace(`Redis Delete Value Node: Input - ${JSON.stringify(inputData)}`);
+        await connectorData.client.del(inputData.key);
+		logger.trace(`Redis Delete Value Node: Delete key successful.`);
+    } catch (error) {
+        logger.error(`Redis Delete Value Node: Error deleting key in Redis: ${error}`);
+        throw {
+            "code": "REDIS_DELETE_VALUE_ERROR",
+            "message": "Error deleting key in Redis",
+            "stackTrace": error
+        };
+    }
+};
