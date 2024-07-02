dfcaa9422dbc046b9006b276b5d67186996c137d
nimesh_dools <nimesh@datanimbus.com>
2024-07-02 15:45:45 -0500

    redis set key node

---
+++ Redis/node.setKey.js
@@ -0,0 +1,20 @@
+let logger = global.logger;
+
+module.exports = async (connectorData, inputData) => {
+    logger.trace("Redis Set Value Node: Invoked!");
+    try {
+        logger.trace(`Redis Set Value Node: Input - ${JSON.stringify(inputData)}`);
+		const setArguments = [inputData.key, inputData.value];
+		if (inputData.expiry) {
+			setArguments.push('EX', inputData.expiry);
+		}
+        await connectorData.client.set(...setArguments);
+    } catch (error) {
+        logger.error(`Redis Set Value Node: Error setting value in Redis: ${error}`);
+        throw {
+            "code": "REDIS_SET_VALUE_ERROR",
+            "message": "Error setting value in Redis",
+            "stackTrace": error
+        };
+    }
+};
