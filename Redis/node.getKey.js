4e0f45e73280ad52ec457dd1cd500e07dba1fb13
nimesh_dools <nimesh@datanimbus.com>
2024-07-02 15:45:56 -0500

    redis get key node

---
+++ Redis/node.getKey.js
@@ -0,0 +1,18 @@
+let logger = global.logger;
+
+module.exports = async (connectorData, inputData) => {
+    logger.trace("Redis Get Value Node: Invoked!");
+    try {
+        logger.trace(`Redis Get Value Node: Input - ${JSON.stringify(inputData)}`);
+        const result = await connectorData.client.get(inputData.key);
+        logger.trace(`Redis Get Value Node: Get Value result - ${result}`);
+		return result;
+    } catch (error) {
+        logger.error(`Redis Get Value Node: Error getting value in Redis: ${error}`);
+        throw {
+            "code": "REDIS_GET_VALUE_ERROR",
+            "message": "Error getting value in Redis",
+            "stackTrace": error
+        };
+    }
+};
